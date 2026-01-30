1e3e6df6095f43e075d8c9be6ac15d98d647cc9b
Monodess <margfritadoni@gmail.com>
2026-01-30 19:10:48 +0200

    minimAL FASTAPI added

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import  Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"Hello": "Space"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None]):
+    return {"item_id": item_id, "q": q}
+
+
