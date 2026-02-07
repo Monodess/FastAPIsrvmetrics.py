413e8e46e43bb503b2278cd45fe54c5a41b6b423
Monodess <margfritadoni@gmail.com>
2026-02-07 15:48:01 +0200

    plan making

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+    Pagespeed server metrics Proxy-Worker
+
+Creating minimal api
+for accessing what?
+1) endpoint post url (to capture metrics from)
+2) endpoint get metrics (what contains inputs body?)
+    only 2 endp for reading and writing
+
+
+"""
+
+@app
